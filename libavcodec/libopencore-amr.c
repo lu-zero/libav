70d03a643c1e2ced4e3ca0250e2202fe2bf179ed
Anton Khirnov <anton@khirnov.net>
2013-05-07 07:20:32 +0200

    libopencore-amr: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/libopencore-amr.c
+++ libavcodec/libopencore-amr.c
@@ -35,13 +35,13 @@ static int amr_decode_fix_avctx(AVCodecContext *avctx)
 
     avctx->sample_rate = 8000 * is_amr_wb;
 
-    if (avctx->channels > 1) {
+    if (avctx->ch_layout.nb_channels > 1) {
         avpriv_report_missing_feature(avctx, "multi-channel AMR");
         return AVERROR_PATCHWELCOME;
     }
 
-    avctx->channels       = 1;
-    avctx->channel_layout = AV_CH_LAYOUT_MONO;
+    av_channel_layout_uninit(&avctx->ch_layout);
+    avctx->ch_layout      = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
     avctx->sample_fmt     = AV_SAMPLE_FMT_S16;
     return 0;
 }
@@ -196,7 +196,7 @@ static av_cold int amr_nb_encode_init(AVCodecContext *avctx)
         return AVERROR(ENOSYS);
     }
 
-    if (avctx->channels != 1) {
+    if (avctx->ch_layout.nb_channels != 1) {
         av_log(avctx, AV_LOG_ERROR, "Only mono supported\n");
         return AVERROR(ENOSYS);
     }
