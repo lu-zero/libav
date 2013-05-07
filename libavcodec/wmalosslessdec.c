7033eee46b07d3ba8a82d376feb7b920501dae62
Anton Khirnov <anton@khirnov.net>
2013-05-07 07:20:32 +0200

    wmalosslessdec: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/wmalosslessdec.c
+++ libavcodec/wmalosslessdec.c
@@ -221,8 +221,19 @@ static av_cold int decode_init(AVCodecContext *avctx)
                                                           3, s->decode_flags);
     av_assert0(s->samples_per_frame <= WMALL_BLOCK_MAX_SIZE);
 
+    if (channel_mask) {
+        av_channel_layout_uninit(&avctx->ch_layout);
+        av_channel_layout_from_mask(&avctx->ch_layout, channel_mask);
+    }
+    if (avctx->ch_layout.nb_channels > WMALL_MAX_CHANNELS) {
+        avpriv_request_sample(avctx,
+                              "More than %d channels", WMALL_MAX_CHANNELS);
+        return AVERROR_PATCHWELCOME;
+    }
+    s->num_channels = avctx->ch_layout.nb_channels;
+
     /* init previous block len */
-    for (i = 0; i < avctx->channels; i++)
+    for (i = 0; i < s->num_channels; i++)
         s->channel[i].prev_block_len = s->samples_per_frame;
 
     /* subframe info */
@@ -241,33 +252,14 @@ static av_cold int decode_init(AVCodecContext *avctx)
         return AVERROR_INVALIDDATA;
     }
 
-    s->num_channels = avctx->channels;
-
     /* extract lfe channel position */
-    s->lfe_channel = -1;
-
-    if (channel_mask & 8) {
-        unsigned int mask;
-        for (mask = 1; mask < 16; mask <<= 1)
-            if (channel_mask & mask)
-                ++s->lfe_channel;
-    }
-
-    if (s->num_channels < 0) {
-        av_log(avctx, AV_LOG_ERROR, "invalid number of channels %"PRId8"\n",
-               s->num_channels);
-        return AVERROR_INVALIDDATA;
-    } else if (s->num_channels > WMALL_MAX_CHANNELS) {
-        avpriv_request_sample(avctx,
-                              "More than %d channels", WMALL_MAX_CHANNELS);
-        return AVERROR_PATCHWELCOME;
-    }
+    s->lfe_channel = av_channel_layout_channel_index(&avctx->ch_layout,
+                                                     AV_CHAN_LOW_FREQUENCY);
 
     s->frame = av_frame_alloc();
     if (!s->frame)
         return AVERROR(ENOMEM);
 
-    avctx->channel_layout = channel_mask;
     return 0;
 }
 
