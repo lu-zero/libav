6e05b2d21f40415e8b78a1789e2b81783c518b8b
Anton Khirnov <anton@khirnov.net>
2013-05-07 07:20:32 +0200

    libilbcdec: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavcodec/libilbc.c
+++ libavcodec/libilbc.c
@@ -66,8 +66,8 @@ static av_cold int ilbc_decode_init(AVCodecContext *avctx)
 
     WebRtcIlbcfix_InitDecode(&s->decoder, mode, s->enhance);
 
-    avctx->channels       = 1;
-    avctx->channel_layout = AV_CH_LAYOUT_MONO;
+    av_channel_layout_uninit(&avctx->ch_layout);
+    avctx->ch_layout      = (AVChannelLayout)AV_CHANNEL_LAYOUT_MONO;
     avctx->sample_rate    = 8000;
     avctx->sample_fmt     = AV_SAMPLE_FMT_S16;
 
