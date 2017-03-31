cc36a5dcb53c7c5e049488451b06466bb521bfb4
Vittorio Giovara <vittorio.giovara@gmail.com>
2017-03-31 13:48:50 +0200

    eac: convert to new channel layout API
    Signed-off-by: Vittorio Giovara <vittorio.giovara@gmail.com>

--- libavformat/eacdata.c
+++ libavformat/eacdata.c
@@ -71,7 +71,7 @@ static int cdata_read_header(AVFormatContext *s)
     st->codecpar->codec_type = AVMEDIA_TYPE_AUDIO;
     st->codecpar->codec_tag = 0; /* no fourcc */
     st->codecpar->codec_id = AV_CODEC_ID_ADPCM_EA_XAS;
-    st->codecpar->channels = cdata->channels;
+    av_channel_layout_default(&st->codecpar->ch_layout, cdata->channels);
     st->codecpar->sample_rate = sample_rate;
     avpriv_set_pts_info(st, 64, 1, sample_rate);
 
