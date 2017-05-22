6fdbfa5829b4b92a4c1e16e5e3b296d4d5d2ef16
wm4 <nfxjfg@googlemail.com>
2017-05-22 13:51:08 +0200

    avconv: remove old avconv_dxva2
    This is done by the generic code now. It was not removed earlier to
    reduce the diff in the commit adding the new code.

--- avtools/avconv_opt.c
+++ avtools/avconv_opt.c
@@ -64,11 +64,9 @@ const HWAccel hwaccels[] = {
     { "d3d11va", hwaccel_decode_init, HWACCEL_D3D11VA, AV_PIX_FMT_D3D11,
       AV_HWDEVICE_TYPE_D3D11VA },
 #endif
-#if HAVE_DXVA2_LIB
+#if CONFIG_DXVA2
     { "dxva2", hwaccel_decode_init, HWACCEL_DXVA2, AV_PIX_FMT_DXVA2_VLD,
       AV_HWDEVICE_TYPE_DXVA2 },
-    { "dxva2-old", dxva2_init, HWACCEL_DXVA2_OLD, AV_PIX_FMT_DXVA2_VLD,
-      AV_HWDEVICE_TYPE_NONE },
 #endif
 #if CONFIG_VDA
     { "vda",   vda_init,   HWACCEL_VDA,   AV_PIX_FMT_VDA,
